5d7de6af38cc5671a932edac74c63f0ec2d37b57
再頒布可能幼女 <a32real@yahoo.co.jp>
2016-11-18 05:48:57 +0900

    ねむい

---
+++ RedefinableLauncher.InfoEditor/Forms/EditForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Redefinable.Applications.Launcher.InfoEditor.Forms
+{
+    public class EditForm : Form
+    {
+        // 非公開フィールド :: コントロール
+
+
+        // コンストラクタ
+
+        public EditForm()
+        {
+
+        }
+
+
+        // 非公開メソッド
+
+        private void _initializeControls()
+        {
+
+        }
+    }
+}
---
+++ RedefinableLauncher.InfoEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("Redefinable Launcher Config and Registration tool")]
+[assembly: AssemblyDescription("Redefinable System LauncherSystem Config and Registration tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Explorers of the Binary World")]
+[assembly: AssemblyProduct("Redefinable Launcher")]
+[assembly: AssemblyCopyright("Copyright © 2007-2016 Explorers of the Binary World All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、 
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("9284ad8d-140c-414c-acbf-e8981d3fe23a")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+[assembly: AssemblyVersion("0.3.11.4")]
+[assembly: AssemblyFileVersion("0.3.11.4")]
